e0856b0e73eb3563d16a4fbdf895ff3ac29b0796
pureye4u <pureye4u@gmail.com>
2016-12-01 17:58:04 +0900

    Test ng2material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 
-import { AppComponent } from './app.component';
+import { AppComponent, DialogContent } from './app.component';
+import 'hammerjs';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        MaterialModule.forRoot()
+    ],
+    declarations: [
+        AppComponent,
+        DialogContent
+    ],
+    entryComponents: [
+        DialogContent
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
